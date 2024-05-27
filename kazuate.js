056704e42193146bd2753890587cea8bc880d70b
G384052023 <238405@st.takushoku-u.ac.jp>
2024-05-27 13:49:43 +0900

    hentai

--- kazuate.js
+++ kazuate.js
@@ -9,13 +9,12 @@ let yoso = document.querySelector('input[name="num"]');
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+
+let b1 = document.querySelector('button#print');
+b1.addEventListener('click', hentai);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
+function hentai() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   // kotae と yoso が一致するかどうか調べて結果を出力
     console.log(counter+'回目の予想：'+yoso);
